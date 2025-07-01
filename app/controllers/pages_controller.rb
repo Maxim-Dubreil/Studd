58ba6b7792c4b8e5e01c2119b8341753eb0cd6e9
Adn <yannis.djaffer@gmail.com>
2025-07-01 19:20:50 +0200

    Need to enabled turbo and hotwire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  def landingPage
+  def landing_page
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "pages#landingPage"
+  root "pages#landing_page"
 end
