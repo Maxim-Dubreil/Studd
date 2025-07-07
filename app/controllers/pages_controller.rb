f257af96f3c6848c0170982f860188d19fe60613
Maxim <m.dubreil25@gmail.com>
2025-07-07 17:17:28 +0200

    refactor: adjust authentication callbacks in controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:landing_page, :about]
   def landing_page
 
     @hide_sidebar = true
     @hide_navbar = true
+
   end
 
   def about
